27f8b23c8f7613f218a3d22973c4ea11e37f3b24
BrentonKasler <144177990+BrentonKasler@users.noreply.github.com>
2023-10-26 23:21:19 -0400

    Add files via upload

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Brenton Kasler
+ *        Date: 10-26-23
+ * Description: git-lab2 c++ code
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!!" << endl;
+    return 0;
+}// main
